c3bead32a9af98608a65727e1a30a226aef6f343
Dan Cleary <dcc635@gmail.com>
2014-01-04 23:19:33 -0800

    Completed up to tastypie docs 1.6

--- mopomo_django/mopomo_django/api.py
+++ mopomo_django/mopomo_django/api.py
@@ -1,7 +1,16 @@
+from django.contrib.auth.models import User
+from tastypie import fields
 from tastypie.resources import ModelResource
 from mopomo_django.models import Timer
 
+class UserResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user'
+
 class TimerResource(ModelResource):
+    user = fields.ForeignKey(UserResource, 'user')
+
     class Meta:
         queryset = Timer.objects.all()
         resource_name = 'timer'
