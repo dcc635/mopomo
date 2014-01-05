8013a4627fc3a97ed6b8c83a4fafa4c987b00411
Dan Cleary <dcc635@gmail.com>
2014-01-04 23:48:53 -0800

    API has basic authentication

--- mopomo_django/mopomo_django/api.py
+++ mopomo_django/mopomo_django/api.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from tastypie import fields
+from tastypie.authentication import BasicAuthentication
 from tastypie.resources import ModelResource
 from mopomo_django.models import Timer
 
@@ -7,6 +8,15 @@ class UserResource(ModelResource):
     class Meta:
         queryset = User.objects.all()
         resource_name = 'user'
+        excludes = [
+            'email',
+            'password',
+            'is_active',
+            'is_staff',
+            'is_superuser',
+        ]
+        allowed_methods = ['get']
+        authentication = BasicAuthentication()
 
 class TimerResource(ModelResource):
     user = fields.ForeignKey(UserResource, 'user')
--- mopomo_django/mopomo_django/urls.py
+++ mopomo_django/mopomo_django/urls.py
@@ -1,16 +1,15 @@
-from django.conf.urls import patterns, include, url
-from mopomo_django.api import TimerResource
+from django.conf.urls import patterns, url, include
+from tastypie.api import Api
+from mopomo_django.api import TimerResource, UserResource
 
 from django.contrib import admin
 admin.autodiscover()
 
-timer_resource = TimerResource()
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(TimerResource())
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mopomo_django.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^api/', include(timer_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
