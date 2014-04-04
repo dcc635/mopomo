56173e89a2f389e101339d51cc339d3b1d60c385
Dan Cleary <dan@cleary.io>
2014-04-03 22:34:25 -0700

    Got nice looking digit output

--- django_/mopomo_django/api.py
+++ django_/mopomo_django/api.py
@@ -29,26 +29,26 @@ class CORSResource(ModelResource):
         response['Access-Control-Allow-Origin'] = '*'
         response['Access-Control-Allow-Headers'] = 'Content-Type'
         return response
- 
+
     def method_check(self, request, allowed=None):
         if allowed is None:
             allowed = []
- 
+
         request_method = request.method.lower()
         allows = ','.join(map(str.upper, allowed))
- 
+
         if request_method == 'options':
             response = HttpResponse(allows)
             response['Access-Control-Allow-Origin'] = '*'
             response['Access-Control-Allow-Headers'] = 'Content-Type'
             response['Allow'] = allows
             raise ImmediateHttpResponse(response=response)
- 
+
         if not request_method in allowed:
             response = http.HttpMethodNotAllowed(allows)
             response['Allow'] = allows
             raise ImmediateHttpResponse(response=response)
- 
+
         return request_method
 
 
